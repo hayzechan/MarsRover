519703b6226d55fb490ae0772106c7a5db1bed65
hayzechan <clonghei101899@gmail.com>
2021-10-21 16:47:03 +0800

    [Mars][Hayze] test: test case for location 0-1S created

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -23,6 +23,21 @@ class MarsRoverTest {
         String actual = marsRover.executeCommand(command);
         //then
         assertEquals(expected,actual);
+    }
+    @Test
+    void shouldMoveForwardFromS_whenExecuteCommand_givenStartFromS() {
+        //given
+        int locationX = 0;
+        int locationY = 0;
+        String direction = "S";
+        String command = "M";
+        String expected = "0 -1 S";
 
+        MarsRover marsRover = new MarsRover(locationX, locationY, direction);
+
+        //when
+        String actual = marsRover.executeCommand(command);
+        //then
+        assertEquals(expected,actual);
     }
 }
